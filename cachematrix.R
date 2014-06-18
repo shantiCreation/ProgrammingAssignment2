8a5e9f5caa0511584ac956df3c9da23422eb0477
shantiCreation <oshmali@yahoo.com>
2014-06-18 16:45:18 -0400

    finished second function, added comments on how to test both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,7 @@
 ## This file contains two functions:
-## makeCacheMatrix and casheSolve. 
+## makeCacheMatrix and cacheSolve. 
 ##
 ## makeCacheMatrix will create matrix object that have built-in methods.
-## This is anolougous to creating a class with methods in java. Once the object is created,
-## it has methods to set and retrieve its values/properties.
 ##
 ## casheSolve function will always check weather inverse matrix already exists,
 ## if it does it will return previously stored inverse matrix, if not it will 
@@ -18,12 +16,13 @@
 ## 4.   getinverse  - returns inverse matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-        ## setting variable,inv, to store value of inverse matrix 
+        
+        ## declaring and initiating inv variable to store value of inverse matrix 
         inv <- NULL
         
-        ## if y$set is called, it will set a new matrix y instead of x when we
-        ## first called makeCacheMatrix function passing x matrix as an argument.
-        ## At the same time we are resetting inv variable that stored inverse matrix to NULL
+        ## set function will be called as x$set(y); it will set a new matrix y instead of x when we
+        ## call makeCacheMatrix with x as an argument.
+        ## At the same time we are re-initiating inv variable that stores inverse matrix to NULL
         set <- function(y) {
                 
                 x <<- y
@@ -32,32 +31,43 @@ makeCacheMatrix <- function(x = matrix()) {
         ## returns matrix
         get <- function() x 
         
-        ## this one will store inverse matrix, so it could be retrieved with getinverse method
+        ## stores inverse matrix, so it could be retrieved with getinverse method
         setinverse <- function(inverse) inv<<- inverse
         getinverse <- function() inv
         
-        ## setting names of the available functions within makeCasheMatrix that could be called
-        ## when we build a matrix with makeCasheMatrix function
+        ## returns list of functions with definitions
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 
-
-
 }
 
 
-## Still working on this function...
+## This function is used to check whether inverse matrix has been already calculated and stored
+## if not, then it will call solve() to calculate inverse function and return it
 
 cacheSolve <- function(x, ...) {
+        
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getmean()
-        if(!is.null(m)) {
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
                 message("getting cached data")
-                return(m)
+                return(inv)
         }
         data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
+
+## ---------- How To Test -------------------------------
+## If you like to test these functions here are some steps:
+## 1.Source cachematrix.R file 
+## 2. In your work console create square matrix: m1 <- matrix (1:4, 2,2)
+## 3. initialize a new matrix by calling makeCacheMatrix with m1 arugment:
+## m2 <- makeCacheMatrix(m1)
+## 4. check what is m2 by using m2$get()
+## 5. calculate inverse matrix by calling cacheSolve(m2), it will print matrix
+## 6. Check if you can retreive inverse matrix for m2:
+## m2$getinverse()
+## 
